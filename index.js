c26a979853fdb623ce1d88827c439f5a0bd33f79
Victoria <vmcdaniel459@gmail.com>
2024-06-17 16:22:14 -0400

    complete

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
 // Write your solution here!
+const cats = ['Milo', 'Otis', 'Garfield'];
+//cats.push('Ralph')
+function destructivelyAppendCat(name) {
+    cats.push('Ralph');
+}
+function destructivelyPrependCat(name) {
+    cats.unshift('Bob');
+}
+function destructivelyRemoveLastCat(name) {
+    cats.pop('Ralph');
+}
+function destructivelyRemoveFirstCat(name) {
+    cats.shift('Milo');
+}
+function appendCat(name) {
+    const newCats = [...cats, 'Broom'];
+    return newCats;
+}
+function prependCat(name) {
+    const newCatTwo = ['Arnold', ...cats];
+    return newCatTwo;
+}
+
+function removeLastCat(name) {
+    const LastCat = cats.slice(0,-1);
+    return LastCat;
+}
+
+function removeFirstCat(name) {
+    const firstCat = cats.slice(1);
+    return firstCat;
+}
+
+
+
+// remove last element of an array: slice(0,-1)
+//remove first element of an array: slice(1)
+
+
+
+
+
+
+
+
+
+
+
